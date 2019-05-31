07a27cfd97acfd280150e0f81f10823b85ca3813
Jeff Andrews <jeff@paintedbyjeff.com>
2019-05-31 13:04:43 -0400

    work 1700 053019
    psuedocode moved around to reflect
    new logic. Gamestates added in
    anticipation of changing only
    the gamecontent area of the DOM

--- piratefarkle.js
+++ piratefarkle.js
@@ -1,77 +1,76 @@
-//variables
+//gameState0/start screen/global functions
+    //reset
+    function gameState0() {
 
-let hand = [6, 6, 6, 6, 6];//default hand will be all jollyrogers.
-let players = [];//function - each player: points object must be in this array
-// and only an array can index each player?
-let currentPoints = 0;
-let currentPlayer = 0;
+    }
+    //rules function
+    function rules() {
+        alert('these are the rules!');
+    }
 
+//gameState1
+    function gameState1() {
+        //how many players? If only one player, alert 'this game requires two or more players', reset input.
+        //player 1 name, set score to 0, add to object or array of players
+        //repeat until number of players set
+        //play button
+    }
 
+//gameState2
+    //roll high score to determine first player to go.
+    //result of high score changes value of currentPlayer, this player starts game and then it's sequential.
 
-//faces - there are six. We need a value, a face and a url for each.
+//gameState3
+    //play game, farkle or add points until loss
+    //repeat for each player until 10000
 
+    //Count round 1. play screen first player, six die,  hit roll, all die produce random face.
 
-//functions
+    for (round = 1; playerScore.any < 10000; round++) {
 
-    //die function
-    //switch might be a good way to handle each of the six cases.
-
-    switch (dieValue) {
-        case 1: 
-        //display 1, check to see if
 
     }
 
-
-
-//display titlescreen. Rules button. Play button.
-
-    //when play button is hit, ask for name of player one. Start, or more players, or quit? If only one player,
-    //alert 'this game requires two or more players'.
-    //When done, Store player one in array. Set score to zero. ask for name of player two. 
-    //for as long as there are more players, run the above in a loop.
-
-
-//roll high score to determine first player to go.
-
-//result of high score changes value of currentPlayer, this player starts game and then it's sequential.
-
-for (round = 1; playerScore.any < 10000; round++) {
-
-
-//Count round 1. play screen first player, six die,  hit roll, all die produce random face.
+    //variables
+    let hand = [6, 6, 6, 6, 6];//default hand will be all jollyrogers.
+    let players = [];//function - each player: points object must be in this array
+    // and only an array can index each player?
+    let currentPoints = 0;
+    let currentPlayer = 0;
 
 
 
-//Click die you want to keep, and hit roll again, or hit end round and keep points.
+    //functions
+
+    //faces - there are six. We need a value, a face and a url for each.
+
+    //Click die you want to keep, and hit roll again, or hit end round and keep points.
     
-//tally kept die at start of new roll, and add to running total
+    //tally kept die at start of new roll, and add to running total
 
-//if all six die produce points, keep going until keep points or lose points.
-    
-//tally die at end of round and if > 0, add to player score
-
-
-
-//go to next player. repeat above.
+    //if all six die produce points, keep going until keep points or lose points.
+        
+    //tally die at end of round and if > 0, add to player score
 
 
 
-//repeat above until no more players.
+    //go to next player. repeat above.
 
 
 
-//Count another round.
-
-
-}
-
-//if any player's score reaches 10000, show win screen.
+    //repeat above until no more players.
 
 
 
+    //Count another round.
 
+    //if any player's score reaches 10000, show win screen.
 
+//gameState3 
+
+    //show final scores
+    //so and so wins
+    //play again?
 
 
 
@@ -87,12 +86,4 @@ for (round = 1; playerScore.any < 10000; round++) {
 
 
 
-
-
-
-
-
-//if any player's score reaches 10000, end game and tell the screen that player won
-
-//ask "another game?"
 
---
+++ piratefarklecodeplayground.js
@@ -0,0 +1,18 @@
+// just for messing about 
+//player should be an object that has a name and a score;
+
+class Player {
+    constructor(){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+
+class PirateFarkleGame {
+    constructor(){
+        this.players = players;
+
+    }
+}
+
